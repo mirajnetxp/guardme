c311c143f270251e18eb88f74486ce0bb545ac39
miraj <miraj.net.xp@gmail.com>
2018-05-27 14:36:47 +0600

    Jobs awarded (if user is freelancer)

--- local/app/Http/Controllers/Api/FreelancerJobsController.php
+++ local/app/Http/Controllers/Api/FreelancerJobsController.php
@@ -56,4 +56,16 @@ class FreelancerJobsController extends Controller {
 
 	}
 
+	public function awardedJobs() {
+		$ID            = auth()->user()->id;
+		$awardedJobs = DB::table( 'job_applications' )
+		                   ->where( 'applied_by', $ID )
+		                   ->where( 'is_hired', 1 )
+							->Join( 'security_jobs', 'job_applications.job_id', '=', 'security_jobs.id' )
+							->select('job_applications.job_id','security_jobs.title','job_applications.updated_at')
+							->get();
+
+		return response()->json($awardedJobs,200);
+	}
+
 }

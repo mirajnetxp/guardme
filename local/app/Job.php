217218e62f6a6424ffa3bd8d2ecfaaf578c7d882
Cassy <cokwedadi@gmail.com>
2018-06-03 09:35:23 +0100

    Revert "Revert "Point four done""
    This reverts commit 14d772bc1876d7e1f7f446045b9f81d08da2dd43.

--- local/app/Job.php
+++ local/app/Job.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 use Responsive\SecurityJobsSchedule;
 
-<<<<<<< HEAD
 class Job extends Model {
 	/**
 	 * The table associated with the model.
@@ -137,130 +136,6 @@ class Job extends Model {
 		*/
 		$distance_select_sub = sprintf(
 			"           
-=======
-class Job extends Model
-{
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'security_jobs';
-    public function schedules() {
-        return $this->hasMany(SecurityJobsSchedule::class);
-    }
-    public static function calculateJobAmount($id) {
-        $job = Job::find($id);
-        $return_data = self::calculateJobAmountWithJobObject($job);
-        return $return_data;
-    }
-
-    /**
-     * @param Job $job
-     * @return array
-     */
-    public static function calculateJobAmountWithJobObject(Job $job) {
-        $number_of_freelancers = $job->number_of_freelancers;
-        $working_hours = ($job->daily_working_hours) * $number_of_freelancers;
-        $working_days = $job->monthly_working_days;
-        $pay_per_hour = $job->per_hour_rate;
-        $total_working_hours = $working_hours * $working_days;
-        $basic_total = $total_working_hours * $pay_per_hour;
-        $vat_fee = number_format(($basic_total * 20) / 100, 2, '.', '');
-        $admin_fee = number_format(($basic_total * 14.99) / 100, 2, '.', '');
-        $grand_total = $basic_total + $vat_fee + $admin_fee;
-        $grand_total = number_format($grand_total, 2, '.', '');
-        $return_data = [
-            'daily_working_hours' => $working_hours,
-            'monthly_working_days' => $working_days,
-            'per_hour_rate' => $pay_per_hour,
-            'total_working_hours_per_month' => $total_working_hours,
-            'basic_total' => floatval($basic_total),
-            'vat_fee' => floatval($vat_fee),
-            'admin_fee' => floatval($admin_fee),
-            'grand_total' => floatval($grand_total),
-            'number_of_freelancers' => $number_of_freelancers,
-            'single_freelancer_fee' => floatval($basic_total/$number_of_freelancers)
-        ];
-        return $return_data;
-    }
-
-    /**
-     * @return \Illuminate\Support\Collection
-     * 
-     */
-    public static function getMyJobs() {
-    	//  schedules added by miraj
-        $user_id = auth()->user()->id;
-        $data = Job::with(['poster','poster.company','industory','schedules','applications'])
-                   ->where('created_by', $user_id)->get();
-        return $data;
-    }
-
-    public static function findJobs() {
-        $jobs = Job::where('status', 1)
-            ->orderBy('id', 'DESC')
-            ->get();
-        return $jobs;
-    }
-    
-    /*
-     *  find the n closest locations
-     *  @param Model $query eloquent model
-     *  @param float $max_distance distance in miles or km
-     *  @param string $units miles or kilometers
-     *  @param Array $fiels to return
-     *  @return array
-     */
-    public static function getSearchedJobNearByPostCode($data_arr, $latitude=0, $longitude=0, $max_distance = 600, $units = 'kilometers', $page=1)
-    {        
-        $numPerPage = 10;
-        
-        /*
-        *  Allow for changing of units of measurement
-        */
-        switch ( $units ) {
-            case 'miles':
-                //radius of the great circle in miles
-                $gr_circle_radius = 5;
-            break;
-            case 'kilometers':
-                //radius of the great circle in kilometers
-                $gr_circle_radius = 6371;
-            break;
-        }  
-        
-        //
-        $cat_id = trim($data_arr['cat_id']);
-        // todo: filter location
-        $loc_val = trim($data_arr['loc_val']);
-        // todo: filter keyword
-        $keyword = $data_arr['keyword']; 
-        // todo: filter distance
-        $distance = $data_arr['distance'];
-        if( $distance == 1 ){
-            $min_distance = 0;
-            $max_distance = 10;
-        } else if( $distance == 2 ){
-            $min_distance = 10;
-            $max_distance = 20;
-        } else if( $distance == 3 ){
-            $min_distance = 20;
-            $max_distance = 50;
-        } else if( $distance == 4 ){
-            $min_distance = 50;
-            $max_distance = 600;
-        } else {
-            $min_distance = 0;
-            $max_distance = 600;
-        }
-        
-        /*
-        *  Generate the select field for disctance
-        */
-        $distance_select_sub = sprintf(
-                                   "           
->>>>>>> parent of e3c088e4... Point four done
                                    ROUND(( %d * acos( cos( radians(%s) ) " .
 			" * cos( radians( J.latitude ) ) " .
 			" * cos( radians( J.longitude ) - radians(%s) ) " .

587a579db3de3ebc3082b1b9e3fb9dec7687d5c0
kosal <kosal@working.com>
2018-06-04 20:07:32 +0700

    modify

--- local/app/Http/Controllers/SettingController.php
+++ local/app/Http/Controllers/SettingController.php
@@ -18,29 +18,35 @@ class SettingController extends Controller {
 
 
 	public function show() {
+		$userids =  DB::select('select id from users');
+		$visibles = FreelancerSetting::all();
+		if (count($visibles) == 0) {
+			$query = "";
+			foreach ($userids as $userid) {
+				DB::insert('insert into freelancer_settings(user_id, visible) values ('.$userid->id.', 0);');
+			}
+		}
 		if ( ! Auth::Check() ) {
 			return redirect( '/' );
 		}
-
-		return view( 'setting' );
+		$visible = auth()->user()->freelancerSettings->visible;
+		return view( 'setting', compact('visible'));
 	}
 
 	public function visibality() {
-
-
 		if ( ! Auth::Check() ) {
 			return redirect( '/' );
 		}
-		if ( auth()->user()->freelancerSettings->visible == true ) {
+		if ( auth()->user()->freelancerSettings->visible == 1 ) {
 			DB::table( 'freelancer_settings' )
 			  ->where( 'user_id', auth()->user()->id )
-			  ->update( [ 'visible' => false ] );
+			  ->update( [ 'visible' => 0 ] );
 
 			return response()->json( '102', 200 );
 		} else {
 			DB::table( 'freelancer_settings' )
 			  ->where( 'user_id', auth()->user()->id )
-			  ->update( [ 'visible' => true ] );
+			  ->update( [ 'visible' => 1 ] );
 
 			return response()->json( '101', 200 );
 		}
--- local/resources/views/setting.blade.php
+++ local/resources/views/setting.blade.php
@@ -114,15 +114,21 @@
                                     <td>
                                         <h3>
                                             <label class="switch">
+                                            @if($visible)
                                                 <input id="visibality" name="visibality"
-                                                        type="checkbox">
+                                                            type="checkbox" checked>
+                                            @else
+                                                <input id="visibality" name="visibality"
+                                                            type="checkbox">
+                                            @endif
+                                                
                                                 <div class="slider round"></div>
                                             </label>
                                         </h3>
                                     </td>
                                 </tr>
                             </table>
-                            <div><a href="{{URL::to('delete_account')}}" >Close account</a></div>
+                            <div><a href="{{URL::to('delete_account')}}" class="btn">Close account</a></div>
                         </div>
                     </div>
                 </div>
@@ -196,8 +202,10 @@
 
                 if (d == '101') {
                     alert('Your profile is now public.')
+                    $(this).attr("checked", "true");
                 } else {
                     alert('Your profile is now private.')
+                    $(this).attr("checked", "false");
                 }
             },
             error: function (xhr, textStatus, errorThrown) {

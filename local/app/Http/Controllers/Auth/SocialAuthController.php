525f7d412dfe26f2b84e65f369d2bb11109f12de
miraj <miraj.net.xp@gmail.com>
2018-07-09 08:00:52 +0600

    Fix the social auth issue for fb and google

--- local/app/Http/Controllers/Auth/SocialAuthController.php
+++ local/app/Http/Controllers/Auth/SocialAuthController.php
@@ -12,114 +12,112 @@ use Responsive\Http\Repositories\UsersRepository;
 use Responsive\User;
 
 
-class SocialAuthController extends Controller
-{
-    use AuthenticatesUsers;
+class SocialAuthController extends Controller {
+	use AuthenticatesUsers;
 
-    /**
-     * @var SocialAuthRepository
-     */
-    private $socialAuthRepository;
-    private $socialite;
-    /**
-     * @var UsersRepository
-     */
-    private $usersRepository;
+	/**
+	 * @var SocialAuthRepository
+	 */
+	private $socialAuthRepository;
+	private $socialite;
+	/**
+	 * @var UsersRepository
+	 */
+	private $usersRepository;
 
-    /**
-     * SocialAuthController constructor.
-     * @param SocialAuthRepository $socialAuthRepository
-     * @param UsersRepository $usersRepository
-     */
-    public function __construct(SocialAuthRepository $socialAuthRepository, UsersRepository $usersRepository)
-    {
-        $this->socialAuthRepository = $socialAuthRepository;
-        $this->usersRepository = $usersRepository;
-    }
+	/**
+	 * SocialAuthController constructor.
+	 *
+	 * @param SocialAuthRepository $socialAuthRepository
+	 * @param UsersRepository $usersRepository
+	 */
+	public function __construct( SocialAuthRepository $socialAuthRepository, UsersRepository $usersRepository ) {
+		$this->socialAuthRepository = $socialAuthRepository;
+		$this->usersRepository      = $usersRepository;
+	}
 
 
-    /**
-     * @param $social
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function socialLogin($social)
-    {
-        return Socialite::driver($social)->redirect();
-    }
+	/**
+	 * @param $social
+	 *
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
+	public function socialLogin( $social ) {
+		return Socialite::driver( $social )->redirect();
+	}
 
-    /**
-     * Obtain the user information from Socialite returned information.
-     * @param $provider
-     * @return Response
-     */
-    public function handleProviderCallback($provider)
-    {
-        $userSocial = Socialite::driver($provider)->user();
+	/**
+	 * Obtain the user information from Socialite returned information.
+	 *
+	 * @param $provider
+	 *
+	 * @return Response
+	 */
+	public function handleProviderCallback( $provider ) {
+		$userSocial = Socialite::driver( $provider )->user();
+		$user       = $this->usersRepository->getUserByEmail( $userSocial->getEmail() );
 
-        $user = $this->usersRepository->getUserByEmail($userSocial->getEmail());
 
-        if($user){
-            // log user in
-            return $this->logAndRedirectUser($user);
-        } else {
-            // todo: create a new user
-            // first determine whether user should be a freelancer or an employer
-            $new_user = $this->initUserFromSocialAuthProvider($provider, $userSocial);
+		if ( $user ) {
+			// log user in
+			return $this->logAndRedirectUser( $user );
+		} else {
+			// todo: create a new user
+			// first determine whether user should be a freelancer or an employer
+			$new_user = $this->initUserFromSocialAuthProvider( $provider, $userSocial );
 
-            return view('auth.complete-social-auth', compact('new_user'));
-        }
+			return view( 'auth.complete-social-auth', compact( 'new_user' ) );
+		}
 
-    }
+	}
 
-    public function completeSocialAuth($provider)
-    {
-        $data = \request()->all();
+	public function completeSocialAuth( $provider ) {
+		$data = \request()->all();
 
-        $user = $this->usersRepository->getUserById($data['uid']);
+		$user = $this->usersRepository->getUserById( $data['uid'] );
 
-        $user->admin = $data['usertype'];
+		$user->admin = $data['usertype'];
+		if (isset($data['gender']) ) {
+			$user->gender = $data['gender'];
+		}
+		$user->save();
 
-        $user->save();
+		return $this->logAndRedirectUser( $user );
+	}
 
-        return $this->logAndRedirectUser($user);
-    }
+	private function logAndRedirectUser( $user ) {
+		auth()->guard( 'web' )->login( $user );
 
-    private function logAndRedirectUser($user)
-    {
-        auth()->guard('web')->login($user);
+		if ( auth()->check() && auth()->user()->id == 1 ) {
 
-        if(auth()->check() && auth()->user()->id == 1){
+			return redirect( '/admin' );
+		} else {
+			return redirect( '/account' );
+		}
+	}
 
-            return redirect('/admin');
-        }
-        else
-        {
-            return redirect('/account');
-        }
-    }
+	private function initUserFromSocialAuthProvider( $provider, $social_user ) {
+		if ( ! $social_user ) {
+			return;
+		}
 
-    private function initUserFromSocialAuthProvider($provider, $social_user)
-    {
-        if(!$social_user) return;
+		$user = new User();
+		switch ( $provider ) {
+			case 'facebook':
+				$user->name   = $social_user->getName();
+				$user->email  = $social_user->getEmail();
+				$user->photo  = $social_user->getAvatar();
+				break;
+			case 'google':
+				$user->name   = $social_user->getName();
+				$user->email  = $social_user->getEmail();
+				$user->gender = $social_user->user['gender'];
+				$user->photo  = $social_user->getAvatar();
+				break;
+		}
 
-        $user = new User();
+		$user->save();
 
-        switch ($provider){
-            case 'facebook':
-                $user->name = $social_user->getName();
-                $user->email = $social_user->getEmail();
-                $user->gender = $social_user->user['gender'];
-                $user->photo = $social_user->profileUrl;
-
-                break;
-
-            case 'google':
-
-                break;
-        }
-
-        $user->save();
-
-        return $user;
-    }
+		return $user;
+	}
 }
